2d7be745588574a11c26b38be547558d3fa09ef1
jacob huang <objecthuang@hotmail.com>
2025-12-14 11:45:08 +0800

    init

---
+++ config.rb
@@ -0,0 +1,29 @@
+$num_instances = 6
+$os = "ubuntu2204"
+$subnet = "192.168.8"
+
+$extra_vars = {
+    "helm_enabled": true,
+    "metrics_server_enabled": true,    
+    "kube_proxy_mode": "ipvs",
+    "kube_proxy_strict_arp": true,    
+    "registry_enabled": true,
+    "metallb_enabled": true,
+    "metallb_namespace": "metallb-system",
+    "metallb_config": {
+        "address_pools": {
+            "primary": {
+                "ip_range": [
+                    "192.168.8.200-192.168.8.240"
+                ],
+                "auto_assign": true
+            }
+        
+        },
+        "layer2": ["primary"]
+    },
+    "ingress_nginx_enabled": true,
+    "ingress_nginx_host_network": false,
+    "ingress_publish_status_address": "",
+    "local_path_provisioner_enabled": true
+}
